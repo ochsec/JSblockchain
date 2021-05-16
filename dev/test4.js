04566e5dba1d19b170df761d25fe81d357770457
Chris <cochsenreither@protonmail.com>
2021-05-16 14:44:24 -0700

    test 4 (PoW)

---
+++ dev/test4.js
@@ -0,0 +1,24 @@
+const Blockchain = require('./blockchain');
+const coin = new Blockchain();
+const previousBlockHash = 'OINAIOSDFN09N09ASDNF90N90ASNDF';
+const currentBlockData = [
+    {
+        amount: 101,
+        sender: 'N90ANS90N90ANSDFN',
+        recipient: '9WERGJG3WT0JG0SS7'
+    },
+    {
+        amount: 30,
+        sender: 'GDFS0SDFGU90SDU0SDFGSD',
+        recipient: 'FSDG999JEGR9JSDJ9SDF'
+    },
+    {
+        amount: 200,
+        sender: 'G0DSG08W4H8SRG0H8DFGSFDG',
+        recipient: '099HSF9H8SF9HWQR23234'
+    }
+];
+
+const nonce = 1;
+
+console.log('nonce: ' + coin.proofOfWork(previousBlockHash, currentBlockData));
