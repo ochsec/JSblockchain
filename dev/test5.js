6a679cbfe693f94cb85917a8c7bc9f8accb78bcd
Chris <cochsenreither@protonmail.com>
2021-05-16 14:50:30 -0700

    test 5 (hash with leading zeros)

---
+++ dev/test5.js
@@ -0,0 +1,26 @@
+const Blockchain = require('./blockchain');
+const coin = new Blockchain();
+const previousBlockHash = 'OINAIOSDFN09N09ASDNF90N90ASNDF';
+const currentBlockData = [
+    {
+        amount: 101,
+        sender: 'N90ANS90N90ANSDFN',
+        recipient: '9WERGJG3WT0JG0SS7'
+    },
+    {
+        amount: 30,
+        sender: 'GDFS0SDFGU90SDU0SDFGSD',
+        recipient: 'FSDG999JEGR9JSDJ9SDF'
+    },
+    {
+        amount: 200,
+        sender: 'G0DSG08W4H8SRG0H8DFGSFDG',
+        recipient: '099HSF9H8SF9HWQR23234'
+    }
+];
+
+const nonce = coin.proofOfWork(previousBlockHash, currentBlockData)
+
+console.log('nonce: ' + nonce);
+
+console.log('hash with leading zeros: ' + coin.hashBlock(previousBlockHash, currentBlockData, nonce));
