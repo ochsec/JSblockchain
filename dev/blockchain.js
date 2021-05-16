89581c75c07d25f245ec828076212edb7325850c
Chris <cochsenreither@protonmail.com>
2021-05-16 12:47:27 -0700

    test 1

---
+++ dev/blockchain.js
@@ -0,0 +1,22 @@
+function Blockchain() {
+    this.chain = [];
+    this.newTransactions = [];
+}
+
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+    const newBlock = {
+        index: this.chain.length + 1,
+        timestamp: Date.now(),
+        transactions: this.newTransactions,
+        nonce,
+        hash,
+        previousBlockHash
+    };
+
+    this.newTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
+};
+
+module.exports = Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,5 @@
+const Blockchain = require('./blockchain');
+const coin = new Blockchain();
+
+coin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
+console.log(coin);
